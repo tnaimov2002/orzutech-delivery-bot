32e608dc5c9479d84633a363b9e7634c36fed341
To'lqinjon <tnaimov2002@gmail.com>
2025-12-25 15:12:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -203,7 +203,12 @@ async def main():
     )
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+# 🧹 Ushlanmagan xabarlar uchun (INFO logni yo‘qotish)
+@router.message()
+async def catch_all_messages(msg: Message):
+    pass
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 
